b9a10f1e940b02c07d8c85f892a4ef6083f83fe1
Jirapong Nanta <jirapong.nanta@gmail.com>
2011-04-28 14:59:55 +0700

    handle error event on log_connection_exception should be a class method

--- lib/apn_on_rails/app/models/apn/app.rb
+++ lib/apn_on_rails/app/models/apn/app.rb
@@ -1,23 +1,23 @@
 class APN::App < APN::Base
-  
+
   has_many :groups, :class_name => 'APN::Group', :dependent => :destroy
   has_many :devices, :class_name => 'APN::Device', :dependent => :destroy
   has_many :notifications, :through => :devices, :dependent => :destroy
   has_many :unsent_notifications, :through => :devices
   has_many :group_notifications, :through => :groups
   has_many :unsent_group_notifications, :through => :groups
-    
+
   def cert
     (RAILS_ENV == 'production' ? apn_prod_cert : apn_dev_cert)
   end
-  
+
   # Opens a connection to the Apple APN server and attempts to batch deliver
   # an Array of group notifications.
-  # 
-  # 
+  #
+  #
   # As each APN::GroupNotification is sent the <tt>sent_at</tt> column will be timestamped,
   # so as to not be sent again.
-  # 
+  #
   def send_notifications
     if self.cert.nil?
       raise APN::Errors::MissingCertificateError.new
@@ -25,9 +25,9 @@ class APN::App < APN::Base
     end
     APN::App.send_notifications_for_cert(self.cert, self.id)
   end
-  
+
   def self.send_notifications
-    apps = APN::App.all 
+    apps = APN::App.all
     apps.each do |app|
       app.send_notifications
     end
@@ -36,12 +36,12 @@ class APN::App < APN::Base
       send_notifications_for_cert(global_cert, nil)
     end
   end
-  
+
   def self.send_notifications_for_cert(the_cert, app_id)
     # unless self.unsent_notifications.nil? || self.unsent_notifications.empty?
       if (app_id == nil)
         conditions = "app_id is null"
-      else 
+      else
         conditions = ["app_id = ?", app_id]
       end
       begin
@@ -57,15 +57,15 @@ class APN::App < APN::Base
       rescue Exception => e
         log_connection_exception(e)
       end
-    # end   
+    # end
   end
-  
+
   def send_group_notifications
-    if self.cert.nil? 
+    if self.cert.nil?
       raise APN::Errors::MissingCertificateError.new
       return
     end
-    unless self.unsent_group_notifications.nil? || self.unsent_group_notifications.empty? 
+    unless self.unsent_group_notifications.nil? || self.unsent_group_notifications.empty?
       APN::Connection.open_for_delivery({:cert => self.cert}) do |conn, sock|
         unsent_group_notifications.each do |gnoty|
           gnoty.devices.find_each do |device|
@@ -77,9 +77,9 @@ class APN::App < APN::Base
       end
     end
   end
-  
+
   def send_group_notification(gnoty)
-    if self.cert.nil? 
+    if self.cert.nil?
       raise APN::Errors::MissingCertificateError.new
       return
     end
@@ -93,14 +93,14 @@ class APN::App < APN::Base
       end
     end
   end
-  
+
   def self.send_group_notifications
     apps = APN::App.all
     apps.each do |app|
       app.send_group_notifications
     end
-  end          
-  
+  end
+
   # Retrieves a list of APN::Device instnces from Apple using
   # the <tt>devices</tt> method. It then checks to see if the
   # <tt>last_registered_at</tt> date of each APN::Device is
@@ -108,7 +108,7 @@ class APN::App < APN::Base
   # accepting notifications then the device is deleted. Otherwise
   # it is assumed that the application has been re-installed
   # and is available for notifications.
-  # 
+  #
   # This can be run from the following Rake task:
   #   $ rake apn:feedback:process
   def process_devices
@@ -118,7 +118,7 @@ class APN::App < APN::Base
     end
     APN::App.process_devices_for_cert(self.cert)
   end # process_devices
-  
+
   def self.process_devices
     apps = APN::App.all
     apps.each do |app|
@@ -129,23 +129,24 @@ class APN::App < APN::Base
       APN::App.process_devices_for_cert(global_cert)
     end
   end
-  
+
   def self.process_devices_for_cert(the_cert)
     puts "in APN::App.process_devices_for_cert"
     APN::Feedback.devices(the_cert).each do |device|
       if device.last_registered_at < device.feedback_at
         puts "device #{device.id} -> #{device.last_registered_at} < #{device.feedback_at}"
         device.destroy
-      else 
+      else
         puts "device #{device.id} -> #{device.last_registered_at} not < #{device.feedback_at}"
       end
-    end 
+    end
   end
-  
-  
+
+
   protected
   def log_connection_exception(ex)
-    puts ex.message
+    STDERR.puts ex.message
+    raise ex
   end
-    
+
 end
